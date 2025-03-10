6d08695110f5e46e9eebc51ba8d5de39c03aee49
jasm <sierramendezjosealejandro@gmail.com>
2025-03-10 16:40:29 -0400

    Ininital commit

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    server: {
+        hmr: {
+            host: '0.0.0.0', // Esto permite HMR desde fuera del contenedor
+        },
+        port: 3000, // Configura el puerto en el que Vite escucha
+        host: true, // Esto hace que Vite sea accesible desde cualquier IP
+    },
+});
